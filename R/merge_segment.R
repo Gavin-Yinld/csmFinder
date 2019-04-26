690a61c2e8872b0172599ca67f12af385792c810
Gavin-Yinld <38175502+Gavin-Yinld@users.noreply.github.com>
2019-04-26 16:04:08 +0800

    Update merge_segment.R

--- R/merge_segment.R
+++ R/merge_segment.R
@@ -1,4 +1,4 @@
-merge_segment_bulk <- function(csm_detect_output)
+merge_segment_bulk <- function(csm_detect_output,extension=0)
 {
   options(stringsAsFactors=F)
   #require("bedtoolsr")
@@ -7,24 +7,24 @@ merge_segment_bulk <- function(csm_detect_output)
 
   loci_mat <- str_split(loci, ':', n = Inf, simplify = TRUE)
   pos_mat <- str_split(loci_mat[,2], '_', n = Inf, simplify = TRUE)
-  data <- data.frame(chr=loci_mat[,1],start=as.numeric(pos_mat[,1]),end=as.numeric(pos_mat[,4]))
+  data <- data.frame(chr=loci_mat[,1],start=as.numeric(pos_mat[,1])-extension,end=as.numeric(pos_mat[,4])+extension)
   data <- bedtoolsr::bt.sort(data)
   csm_region <- bedtoolsr::bt.merge(data)
   return(csm_region)
 }
 
-merge_segment_single_cell <- function(beta_output)
+merge_segment_single_cell <- function(beta_output,extension=0)
 {
   options(stringsAsFactors=F)
   #require("bedtoolsr")
-  data <- data.frame(chr=beta_output[,1],start=as.numeric(beta_output[,2]),end=as.numeric(beta_output[,3]))
+  data <- data.frame(chr=beta_output[,1],start=as.numeric(beta_output[,2])-extension,end=as.numeric(beta_output[,3])+extension)
   data <- bedtoolsr::bt.sort(data)
   csm_region <- bedtoolsr::bt.merge(data)
   return(csm_region)
 }
 
 
-merge_segment <- function(pCSM_segment,data_type="regular")
+merge_segment <- function(pCSM_segment,data_type="regular",extension=0)
 {
 	options(stringsAsFactors=F)
 	#require("bedtoolsr")
@@ -32,9 +32,9 @@ merge_segment <- function(pCSM_segment,data_type="regular")
 
   if(data_type=="regular")
   {
-    pCSM_loci <- merge_segment_bulk(pCSM_segment)
+    pCSM_loci <- merge_segment_bulk(pCSM_segment,extension=extension)
   } else{
-    pCSM_loci <- merge_segment_single_cell(pCSM_segment)
+    pCSM_loci <- merge_segment_single_cell(pCSM_segment,extension=extension)
   }
   return(pCSM_loci)
 }
