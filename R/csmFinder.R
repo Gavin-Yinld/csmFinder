8939c16aaba198f1d29195ca860ce7af8c702d0a
Gavin-Yinld <38175502+Gavin-Yinld@users.noreply.github.com>
2019-09-15 01:15:32 +0800

    Update csmFinder.R

--- R/csmFinder.R
+++ R/csmFinder.R
@@ -1,6 +1,6 @@
 csmFinder <- function(candidate,data_type='regular',depth=10,distance=0.3,pval=0.05,thread=1){
   if(data_type=='regular'){
-    model_result <- Nonparametric_Bayesian_clustering(candidate,delta= 0.5,tau = 0.5,nperm = 10000,thread=1)
+    model_result <- Nonparametric_Bayesian_clustering(candidate,delta= 0.5,tau = 0.5,nperm = 10000,thread=thread)
     pCSM <- data.frame(candidate,t(model_result))
     pCSM <- pCSM[which(pCSM$d>=distance & pCSM$pval<=pval),]
 
